f30280d45492ed13eda9ad283403196a584e8175
Khalill Hussein <RusObeySwag@gmail.com>
2021-12-04 03:48:19 +0300

    Работа над разделом лекций

---
+++ lib/models/lecture.dart
@@ -0,0 +1,12 @@
+class Lecture {
+  final String number;
+  final String title;
+  final String subtitle;
+  final String fulltext;
+  Lecture({
+    required this.number,
+    required this.title,
+    required this.subtitle,
+    required this.fulltext,
+  });
+}
