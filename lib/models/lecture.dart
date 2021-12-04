abe8c230e301b2aa02b2f6af7bdbd0830c7b7444
Ruslan Badaev <r.badaeff@gmail.com>
2021-12-04 03:53:05 +0300

    fix(app): Fixed Lecture

---
+++ lib/models/lecture.dart
@@ -0,0 +1,13 @@
+class Lecture {
+  final String number;
+  final String title;
+  final String subtitle;
+  final String fulltext;
+
+  Lecture({
+    required this.number,
+    required this.title,
+    required this.subtitle,
+    required this.fulltext,
+  });
+}
